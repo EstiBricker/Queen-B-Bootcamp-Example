019b7a0790c23998fc1c5d3fb938f4fd39566181
EstiBricker <esti.bricker@gmail.com>
2023-11-27 18:01:10 +0200

    connecting React to Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import firstPerson from './images/person1.svg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import firstPerson from "./images/person1.svg";
 const port = process.env.PORT || 5001;
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    axios.get(`http://localhost:${port}/api/helloworld`)
-      .then(response => setMessage(response.data))
-      .catch(error => console.error(`There was an error retrieving the message: ${error}`))
-  }, [])
+    axios
+      .get(`http://localhost:${port}/api/teachers`)
+      .then((response) => setMessage(response.data.message))
+      .catch((error) =>
+        console.error(`There was an error retrieving the message: ${error}`)
+      );
+  }, []);
 
   return (
     <div className="App">
